b5181074146ea0aa13165bede838bcc010034bcc
Mustain Billah Taj <mustainbillahtaj@gmail.com>
2025-06-20 19:18:20 +0600

    first commit

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express=require('express')
+
+require('dotenv').config()
+
+const app=express()
+
+app.use(express.json())
+
+app.listen(process.env.PORT,()=>{
+    console.log('server running on ',process.env.PORT)
+})
