7e17a09461e9cad195fbde44b6f51cb762335ec8
WahidMubarrat <wahidmubarrat20@gmail.com>
2025-07-13 23:34:31 +0600

    save before pull

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -77,7 +77,6 @@ export default function ProfilePage() {
       {userData && (
         <Sidebar
           visible={sidebarVisible}
-          onLogout={handleLogout}
           userData={userData}
         />
       )}
